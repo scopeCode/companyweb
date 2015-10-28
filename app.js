130ad81632b8c0ead84af70ec0e33f0d500fa809
mo <split@live.cn>
2015-10-28 17:28:46 +0800

    封装七牛的 上传 文件

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 
 var app = express();
-
+app.use(multipart());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,31 @@
 var express = require('express');
-var qiniu = require('qiniu');
+var qn = require("../model/qiniu");
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.session.user);
-
+  res.render('index', { title: '首页 '});
+  return;
   if(req.session.user){
     res.render('index', { title: '首页 '});
   }else{
     req.session.error = "请先登录"
     res.redirect('login');
   }
-
 });
-
-qiniu.conf.ACCESS_KEY = 'YYFZ8Mv3gARmlE8-MDc-zj8Yp0p__SoQj6u_Vjuc';
-qiniu.conf.SECRET_KEY = '<Qpmmt-jax55bZxWVC0lLmE__QJgEAfAyGG1CUkVZ';
-
-function uptoken(bucketname) {
-  var putPolicy = new qiniu.rs.PutPolicy(bucketname);
-  return putPolicy.token();
-}
-
-function uploadFile(localFile, key, bucketname) {
-  var extra = new qiniu.io.PutExtra();
-  var _uptoken  = uptoken(bucketname);
-  qiniu.io.putFile(_uptoken, key, localFile, extra, function(err, ret) {
-    if(!err) {
-      console.log(ret.key, ret.hash);
-      // ret.key & ret.hash
-    } else {
-      console.log(err);
-    }
-  });
-}
-
 
 router.post('/upload', function (req,res,next) {
-  console.log('123123123123');
-  uploadFile(req.files['loadFile'],'img/'+req.files['loadFile'].name,'body');
+  var config ={
+    ACCESS_KEY:'YYFZ8Mv3gARmlE8-MDc-zj8Yp0p__SoQj6u_Vjuc',
+    SECRET_KEY:'Qpmmt-jax55bZxWVC0lLmE__QJgEAfAyGG1CUkVZ',
+    BUCKETNAME:'baby',
+  }
+  //(key,filePath,bucketname,success,error)
+  qn.uploadFile("img/"+req.files.uploadFile.name,req.files.uploadFile.path,config,function(ret){
+    res.send(200,{key:ret.key,hash:ret.hash});
+  },function(err){
+    res.send(500,{err:err});
+  });
 });
-
 module.exports = router;
